480a27ab46a3774d2806d791002b37a111a4cfc5
Jonathan Sibley <jonsibley@gmail.com>
2013-02-27 20:04:48 -0800

    Create new HTEmailAutocompleteTextField class

---
+++ Example/HTTextFieldAutocompletionExample/HTEmailAutocompleteTextField.h
@@ -0,0 +1,13 @@
+//
+//  HTEmailAutocompleteTextField.h
+//  HTTextFieldAutocompletionExample
+//
+//  Created by Jonathan Sibley on 2/27/13.
+//  Copyright (c) 2013 Hotel Tonight. All rights reserved.
+//
+
+#import "HTAutocompleteTextField.h"
+
+@interface HTEmailAutocompleteTextField : HTAutocompleteTextField <HTAutocompleteDataSource>
+
+@end
---
+++ Example/HTTextFieldAutocompletionExample/HTEmailAutocompleteTextField.m
@@ -0,0 +1,23 @@
+//
+//  HTEmailAutocompleteTextField.m
+//  HTTextFieldAutocompletionExample
+//
+//  Created by Jonathan Sibley on 2/27/13.
+//  Copyright (c) 2013 Hotel Tonight. All rights reserved.
+//
+
+#import "HTEmailAutocompleteTextField.h"
+
+@implementation HTEmailAutocompleteTextField
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+@end
